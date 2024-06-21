396c0a6a938202c2ba5e1da0138ba64f90469950
ElvinSEM <elvin.seytibramov@gmail.com>
2024-06-21 16:01:24 +0300

    authenticate(авторизация пользователя) commit

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,35 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_05_16_202917) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_09_080446) do
+  create_table "active_storage_attachments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "record_type", null: false
+    t.bigint "record_id", null: false
+    t.bigint "blob_id", null: false
+    t.datetime "created_at", null: false
+    t.index ["blob_id"], name: "index_active_storage_attachments_on_blob_id"
+    t.index ["record_type", "record_id", "name", "blob_id"], name: "index_active_storage_attachments_uniqueness", unique: true
+  end
+
+  create_table "active_storage_blobs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "key", null: false
+    t.string "filename", null: false
+    t.string "content_type"
+    t.text "metadata"
+    t.string "service_name", null: false
+    t.bigint "byte_size", null: false
+    t.string "checksum"
+    t.datetime "created_at", null: false
+    t.index ["key"], name: "index_active_storage_blobs_on_key", unique: true
+  end
+
+  create_table "active_storage_variant_records", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.bigint "blob_id", null: false
+    t.string "variation_digest", null: false
+    t.index ["blob_id", "variation_digest"], name: "index_active_storage_variant_records_uniqueness", unique: true
+  end
+
   create_table "appointments", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.datetime "appointment_date"
     t.string "status"
@@ -22,6 +50,8 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_16_202917) do
     t.integer "age"
     t.string "info"
     t.integer "problem_id"
+    t.datetime "start_time"
+    t.datetime "end_time"
     t.index ["doctor_id"], name: "index_appointments_on_doctor_id"
     t.index ["user_id"], name: "index_appointments_on_user_id"
   end
@@ -30,6 +60,24 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_16_202917) do
     t.string "name"
     t.string "specialty"
     t.string "phone"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.text "description"
+  end
+
+  create_table "reviews", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "doctor_id"
+    t.index ["doctor_id"], name: "index_reviews_on_doctor_id"
+  end
+
+  create_table "services", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price", precision: 10
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -45,6 +93,9 @@ ActiveRecord::Schema[7.0].define(version: 2024_05_16_202917) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
+  add_foreign_key "active_storage_variant_records", "active_storage_blobs", column: "blob_id"
   add_foreign_key "appointments", "doctors"
   add_foreign_key "appointments", "users"
+  add_foreign_key "reviews", "doctors"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,7 +6,7 @@
 
 # Создание массивов пользователей, врачей и паролей
 users = ["user1@mail.com", "user2@mail.com", "user3@mail.com"]
-doctors = ["doctor1@mail.com", "doctor2@mail.com", "doctor3@mail.com"]
+doctors = ["Oftalmology", "Cardiology", "Dermatology"]
 passwords = ["password1", "password2", "password3"]
 phones = ["1234567890", "0987654321", "1122334455"]
 
@@ -24,8 +24,21 @@ end
 
 
 # Создание врачей, если они еще не существуют
+description = ["Врач высшей категории
+Окончил Андижанский медицинский университет в 1986 году. В 1987 году окончил интернатуру по специальности «Офтальмология».
+Врач высшей категории. Заслуженный врач РК.
+Работает в клинике Генезис с 2002 года.
+Конференции по офтальмологии:
+2006 г. — Лондон, Великобритания;
+2007 г. — Верона, Италия;
+2008 г. — Анталия, Турция;
+2009 г. — Барселона, Испания;
+2010 г. — Париж, Франция;
+2011 г. — Вена, Австрия.
+Член Ассоциации офтальмологов Европы, Член Ассоциации офтальмологов Украины.
+2020 год подтверждение квалификации специалиста по направлению подготовки «Офтальмология».", "5 лет специалистом по кардиологии", "5 лет специалистом по дермотологии"]
 doctors.each_with_index do |doctor, index|
-  doctor_objects << Doctor.find_or_create_by!(name: "Doctor #{index + 1}", specialty: doctor, phone: phones[index])
+  doctor_objects << Doctor.find_or_create_by!(name: "Doctor #{index + 1}", specialty: doctor, phone: phones[index], description: description[index])
 end
 
 # Создание записей
@@ -38,5 +51,46 @@ user_objects.each do |user|
       # Теперь при создании записи мы также связываем ее с пользователем и врачом
       Appointment.find_or_create_by!(info: problem, doctor: doctor, user: user)
     end
+  end
+end
+
+
+
+# Добавляем новые услуги
+services = Service.create([
+                            { name: 'Диагностика зрения', description: 'Полная диагностика зрения с использованием современного оборудования', price: 1200.00 },
+                            { name: 'Лечение заболеваний глаз', description: 'Комплексное лечение заболеваний глаз различной сложности', price: 3000.00 },
+                            { name: 'Операции на глаза', description: 'Высокотехнологичные операции на глаза, включая лазерную коррекцию', price: 25000.00 },
+                            { name: 'Коррекция зрения', description: 'Коррекция зрения с использованием современных методик и оборудования', price: 15000.00 }
+                          ])
+
+puts "Добавлено #{services.size} новых услуг."
+
+
+sservices = Service.create([
+                             {
+                               name: Faker::Lorem.sentence(word_count: 3),  # Генерация названия услуги
+                               description: Faker::Lorem.paragraph(sentence_count: 2),  # Генерация описания услуги
+                               price: Faker::Commerce.price  # Генерация цены
+                             },
+                           #...создаем столько услуг, сколько вам нужно
+                           ])
+puts "Добавлено #{services.size} новых услуг."
+
+# Создание общих отзывов о сайте
+3.times do
+  Review.create(
+    name: Faker::Name.name, # Используйте name вместо comment
+    body: Faker::Quote.famous_last_words,
+    )
+end
+
+# Создание отзывов о врачах
+Doctor.all.each do |doctor|
+  2.times do
+    doctor.reviews.create(
+      name: Faker::Name.name, # Используйте name вместо comment
+      body: Faker::Quote.famous_last_words,
+    )
   end
 end
